afb042c4b558a96a23c78ccd28044a218bb7d9a8
oscaremilio <oscaremilio_@hotmail.es>
2020-12-05 22:08:52 +0100

    ejercicio 1_09_1

---
+++ 01-introduccion/Ejercicio_1_09_1.cs
@@ -0,0 +1,15 @@
+// Programa que esribe diversos resultados matemáticos
+// Uso de orden de prioridad de operadores
+// Por oscaremilio
+
+using System;
+
+public class Ejercicio_1_09_1
+{
+    public static void Main()
+    {
+        Console.WriteLine("(20+5) % 6 = {0}", ((20 + 5) % 6));
+        Console.WriteLine("15 +- 5 * 6 / 10 = {0}", 15 +-5 * 6 / 10);
+    }   
+        
+}
