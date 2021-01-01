795516207d63640783dbf6658b875b666263c865
oscaremilio <oscaremilio_@hotmail.es>
2021-01-01 22:58:04 +0100

    Nuevos ejercicios

---
+++ 02-condiciones-y-bucles/Ejercicio_2_08.cs
@@ -0,0 +1,41 @@
+
+/*
+Usando bucles con "for":
+- muestra los números 1 al 20
+- muestra los pares del 2 al 16
+- muestra los impares del 15 al 5, descendiendo.
+*/
+
+
+using System;
+
+class Ejercicio_2_06
+{
+    public static void Main()
+    {
+        for(int i=1; i<=20; i++)
+        {
+            Console.Write("{0} ", i);
+        }
+        
+        Console.WriteLine();
+
+        for(int i=2; i<=16; i++)
+        {
+            if ( (i%2) == 0)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+        
+        Console.WriteLine();
+        
+        for(int i=15; i>=5; i--)
+        {
+            if ( (i%2) != 0)
+            {
+                Console.Write("{0} ", i);
+            }
+        }
+    }  
+}
