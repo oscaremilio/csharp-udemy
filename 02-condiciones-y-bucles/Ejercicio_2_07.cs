ab7ad083967a78d1c051e6e20d10ee5dff9e38f9
oscaremilio <oscaremilio_@hotmail.es>
2021-01-01 22:29:40 +0100

    nuevos ejercicios

---
+++ 02-condiciones-y-bucles/Ejercicio_2_07.cs
@@ -0,0 +1,25 @@
+
+/*
+Programa que pide al usuario una contraseña numérica. Debe repetirse hasta que introduzca "1234", momento en el que responde "Bienvenido".
+Debes usar "Do-While".
+*/
+
+
+using System;
+
+class Ejercicio_2_06
+{
+    public static void Main()
+    {
+        int datoIngresado, contraseña = 1234;
+
+        do
+        {
+            Console.Write("Introduce tu contraseña: ");
+            datoIngresado = Convert.ToInt32( Console.ReadLine() );
+        }
+        while( datoIngresado != contraseña);
+
+        Console.WriteLine("Bienvenido");
+    }  
+}
