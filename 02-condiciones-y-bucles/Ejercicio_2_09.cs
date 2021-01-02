cea79c0735b67f3fe57b1d10b050c1188c9bc35d
oscaremilio <oscaremilio_@hotmail.es>
2021-01-02 23:37:56 +0100

    Ejercicio 2.09

--- 02-condiciones-y-bucles/Ejercicio_2_09.cs
+++ 02-condiciones-y-bucles/Ejercicio_2_09.cs
@@ -1,6 +1,6 @@
 
 /*
-
+Programa que muestra los números del 1 al 20, excepto el 10, usando "continue", y los números del 1 al 20, deteniendolo en 10 usando "break".
 */
 
 
@@ -10,6 +10,27 @@ class Ejercicio_2_09
 {
     public static void Main()
     {
-    
+        for(int i=1; i<=20; i++)
+        {
+            if (i == 10)
+            {
+                continue;
+            }
+
+            Console.Write("{0} ", i);
+        }
+
+        Console.WriteLine();
+
+        for(int i=1; i<=20; i++)
+        {
+            if (i == 10)
+            {
+                break;
+            }
+
+            Console.Write("{0} ", i);
+        }
+
     }  
 }
